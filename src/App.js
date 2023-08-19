b60bba027a4486cb1d71749f5355a69d81819925
manisha <manishab034@gmail.com>
2023-08-19 12:38:07 +0530

    Changed app.js

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
                   <a class="nav-link" href="#">Men</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">Women</a>
+                  <a class="nav-link" href="#">W</a>
                 </li>
                 <li class="nav-item">
                   <a class="nav-link" href="#">About</a>
